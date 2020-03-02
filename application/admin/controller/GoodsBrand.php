78da354fa0f01891a33a94a30955ea98b0eef775
yangxiushan <501942821@qq.com>
2020-03-02 17:07:22 +0800

    goods brand

--- application/admin/controller/GoodsBrand.php
+++ application/admin/controller/GoodsBrand.php
@@ -10,6 +10,7 @@
 namespace app\admin\controller;
 
 use app\common\enum\DbDataIsDeleteEnum;
+use app\common\enum\GoodsBrandStatusEnum;
 use think\Db;
 
 class GoodsBrand extends Common
@@ -85,6 +86,113 @@ class GoodsBrand extends Common
         }
 
         $this->success("添加成功");
+
+    }
+
+    public function edit() {
+
+        $id = input('param.id');
+
+        $info = $this->goodsBrandService->findById($id);
+
+        $this->assign('info', $info);
+
+        return $this->fetch();
+
+    }
+
+
+    public function editPost() {
+
+        $param = input('post.');
+
+        $param["update_time"] = time();
+
+        Db::startTrans();
+        try {
+
+            $this->goodsBrandService->updateByIdAndData($param['id'], $param);
+
+            Db::commit();
+
+        } catch(\PDOException $e) {
+
+            Db::rollback();
+
+            $this->error('服务器错误,请稍后再试');
+
+        }
+
+        $this->success("编辑成功",url("index"));
+    }
+
+    public function delete() {
+
+        $id = input('param.id');
+
+        try {
+            $updateData = [
+                "is_delete" => DbDataIsDeleteEnum::YES,
+                "update_time" => time(),
+            ];
+
+            $result = $this->goodsBrandService->updateByIdAndData($id, $updateData);
+
+            if(false === $result) {
+                $this->error($this->goodsBrandService->getError());
+            }
+
+            $this->success("删除成功", url('index'));
+
+        }catch(\PDOException $e){
+            $this->error($e->getMessage());
+        }
+
+    }
+
+
+    public function activate() {
+
+        $id = input('param.id');
+
+        $goodsBrand = $this->goodsBrandService->findById($id);
+
+        if($goodsBrand['status'] == GoodsBrandStatusEnum::YES) {
+            $this->error("该品牌已是正常使用状态");
+        }
+
+        try {
+
+            $this->goodsBrandService
+                ->updateByIdAndData($id,["status"=>GoodsBrandStatusEnum::YES,"update_time"=>time()]);
+
+            $this->success("已展示");
+
+        } catch (\PDOException $e) {
+            $this->error($e->getMessage());
+        }
+
+    }
+
+    public function deactivate() {
+
+        $id = input('param.id');
+
+        $goodsBrand = $this->goodsBrandService->findById($id);
+
+        if($goodsBrand['status'] == GoodsBrandStatusEnum::FORBIDDEN) {
+            $this->error("该品牌已是禁止使用状态");
+        }
+
+        try {
+            $this->goodsBrandService
+                ->updateByIdAndData($id,["status"=>GoodsBrandStatusEnum::FORBIDDEN,"update_time"=>time()]);
+
+            $this->success("成功禁止");
+
+        } catch (\PDOException $e) {
+            $this->error($e->getMessage());
+        }
 
     }
 }
---
+++ application/common/enum/GoodsBrandStatusEnum.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangxiushan
+ * Date: 2020-03-02
+ * Time: 14:16
+ */
+
+namespace app\common\enum;
+
+class GoodsBrandStatusEnum
+{
+    const YES = 1;
+    const FORBIDDEN = 0;
+}
